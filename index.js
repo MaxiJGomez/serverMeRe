52b539b9a7dd5980b3e2a01214b7ac5092e90a08
Lucas Lopez <lucas.p.lopez.pj@gmail.com>
2023-12-04 10:10:20 -0300

    subo

--- index.js
+++ index.js
@@ -9,24 +9,29 @@ const io = require("socket.io")(server, {
 var listaAlertas = []; // ===> Lista de todas las salas privadas de cada alerta
 
 io.on("connection", (socket) => {
-  socket.on("ubicacion_privada", (data) => {
+  socket.on("creacion_sala", (data) => {
     if (data.tipoApp === "victima") {
-      socket.join(data.idSala); // ===> Crea una sala APP victima
-      agregarAlertas(data); // ===> Agrega las salas creadas a la listas de Salas
+      const codigoSala =generarCodigoUnico(listaAlertas)
+      console.log(codigoSala)
+      socket.join(data.idUsu); // ===> Crea sala privada de la victima
+      socket.join(codigoSala)
+      agregarAlertas({...data, codigoSala}); // ===> Agrega las salas creadas a la listas de Salas
+      io.to(data.idUsu).emit("codigoSala", codigoSala)
     } else {
-      const existeSala = listaAlertas.some((el) => el.idSala === data.idSala); // ===> Comprueba que exista una sala ya creada
+      const existeSala = listaAlertas.some((el) => el.codigoSala === data.codigoSala); // ===> Comprueba que exista una sala ya creada
       if (existeSala) {
-        socket.join(data.idSala); // ===> Se une a una sala existente APP policia
+        socket.join(obtenerIdUsu(data.codigoSala)); // ===> Se une a una sala existente APP policia
       } else {
-        socket.join(data.idSala); // ===> Al no coincidir con el código de la sala de la vícitima, crea una sala  nueva para emitir un mensaje de error y después eliminar la misma sala
-        io.to(data.idSala).emit("ubicacionPrivada", "Código inexistente"); // ===> envía mensaje de error
-        eliminarAlertas(data.idSala);
+        socket.join(data.idUsu); // ===> Al no coincidir con el código de la sala de la vícitima, crea una sala  nueva para emitir un mensaje de error y después eliminar la misma sala
+        io.to(data.idUsu).emit("ubicacionPrivada", "Código inexistente"); // ===> envía mensaje de error
+        eliminarAlertas(data.idUsu);
       }
     }
   });
   socket.on("ubicacion_actual", (data) => {
-    console.log(data);
-    io.to(data.idSala).emit("ubicacionPrivada", data); // ===> envía los datos (lat, long, etc.) del usu a una sala de alerta
+   // console.log(data);
+   
+    io.to(data.idUsu).emit("ubicacionPrivada", data); // ===> envía los datos (lat, long, etc.) del usu a una sala de alerta
   });
 
   socket.on("get_room_list", () => {
@@ -38,21 +43,57 @@ io.on("connection", (socket) => {
     eliminarAlertas(data);
   });
 
-  const eliminarAlertas = (idSala) => {
-    listarAlertas = listaAlertas.filter((alerta) => alerta.idSala !== idSala); // ===> Funcion para eliminar una Sala
+  const eliminarAlertas = (idUsu) => {
+    listarAlertas = listaAlertas.filter((alerta) => alerta.idUsu !== idUsu); // ===> Funcion para eliminar una Sala
     console.log(listaAlertas);
   };
   const agregarAlertas = (nombreAlerta) => {
-    const existe = listaAlertas.some((el) => el.idSala === nombreAlerta.idSala); // ===>controla que el idSala del objeto nuevo no se repita en algún elemento del array listaAlertas
+    const existe = listaAlertas.some((el) => el.idUsu === nombreAlerta.idUsu); // ===>controla que el idSala del objeto nuevo no se repita en algún elemento del array listaAlertas
 
     if (!existe || !listaAlertas) {
       listaAlertas.push({
-        idSala: nombreAlerta.idSala,
+        idUsu: nombreAlerta.idUsu,
         nombre: nombreAlerta.nombre,
+        apellido: nombreAlerta.apellido,
+        codigoSala: generarCodigoUnico(listaAlertas)
       }); // ===> agrega el objeto nuevo al array listaAlertas en caso de que no exista uno con el mismo idSala
       console.log(listaAlertas);
     }
   };
 });
+
+
+const generarCodigoSala =() => {
+  // Generar un número de 4 dígitos al azar
+  const codigoSala = Math.floor(1000 + Math.random() * 9000).toString();
+
+  return codigoSala;
+}
+
+const generarCodigoUnico=(array)=>{
+  let nuevoCodigo;
+  let existeCodigo = false
+
+  do {
+    // Generar un nuevo código de sala
+    nuevoCodigo = generarCodigoSala();
+
+    // Verificar si el nuevo código ya existe en el array
+    existeCodigo = array.some(item => item.codigoSala === nuevoCodigo);
+
+    // Repetir el proceso si el código ya existe
+  } while (existeCodigo);
+
+  return nuevoCodigo;
+}
+
+const obtenerIdUsu =(codigoSala)=>{
+  // Buscar el objeto con el código de sala dado
+  const objetoEncontrado = listaAlertas.find(item => item.codigoSala === codigoSala);
+
+  // Devolver el idUsu si se encontró el objeto, o null si no se encontró
+  return objetoEncontrado.idUsu;
+}
+
 
 server.listen(5050, () => console.log(server.address()));
