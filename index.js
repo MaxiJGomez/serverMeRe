c8e0f97bfbc8f8e16fc1832dd26c83f44c9550ef
MaxiJGomez <mjg_04_10@hotmail.com>
2024-01-04 09:13:27 -0300

    Elimina Salas del socket

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 const http = require("http");
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 10000;
 
 const server = http.createServer();
-
 
 const io = require("socket.io")(server, {
   cors: { origin: "*" },
 });
 
 var listaAlertas = []; // ===> Lista de todas las salas privadas de cada alerta
-var listaPolicias = {}
+var listaPolicias = {};
 
 io.on("connection", (socket) => {
   socket.on("creacion_sala", (data) => {
     if (data.tipoApp === "victima") {
-      const codigoSala =generarCodigoUnico(listaAlertas)
+      const codigoSala = generarCodigoUnico(listaAlertas);
       socket.join(data.idUsu); // ===> Crea sala privada de la victima
-      socket.join(codigoSala)
-      agregarAlertas({...data, codigoSala}); // ===> Agrega las salas creadas a la listas de Salas
-      console.log(listaAlertas)
-      io.to(data.idUsu).emit("codigoSala", codigoSala)
+      socket.join(codigoSala);
+      agregarAlertas({ ...data, codigoSala }); // ===> Agrega las salas creadas a la listas de Salas
+      console.log(listaAlertas);
+      io.to(data.idUsu).emit("codigoSala", codigoSala);
+      io.in(data.idUsu).socketsLeave(data.idUsu); //Elimina la sala inicial del usuario
     } else {
-      const existeSala = listaAlertas.some((el) => el.codigoSala === data.codigoSala); // ===> Comprueba que exista una sala ya creada
+      const existeSala = listaAlertas.some(
+        (el) => el.codigoSala === data.codigoSala
+      ); // ===> Comprueba que exista una sala ya creada
       if (existeSala) {
         socket.join(data.codigoSala); // ===> Se une a sala de victima APP policia
         socket.join(`${data.codigoSala}policia`); // ===> Se une o crea sala paralela a una existente APP policia
-        idUsuario = obtenerIdUsu(data.codigoSala)
-        data.tipoApp === "policia" && idUsuario !== null  ? listaPolicias[idUsuario].policias[data.idDispo] = {ubicacionInicial: {latitud: data.latitud, longitud: data.longitud},ubicacionFinal: {latitud: data.latitud, longitud: data.longitud}} : ()=>{}
-        io.to(data.codigoSala).emit("ubicacionPrivada", obtenerUbiInicial(data.codigoSala))
-        io.to(`${data.codigoSala}policia`).emit("ubicacionPrivadaPolicias", listaPolicias[obtenerIdUsu(data.codigoSala)].policias)
+        idUsuario = obtenerIdUsu(data.codigoSala);
+        data.tipoApp === "policia" && idUsuario !== null
+          ? (listaPolicias[idUsuario].policias[data.idDispo] = {
+              ubicacionInicial: {
+                latitud: data.latitud,
+                longitud: data.longitud,
+              },
+              ubicacionFinal: {
+                latitud: data.latitud,
+                longitud: data.longitud,
+              },
+            })
+          : () => {};
+        io.to(data.codigoSala).emit(
+          "ubicacionPrivada",
+          obtenerUbiInicial(data.codigoSala)
+        );
+        io.to(`${data.codigoSala}policia`).emit(
+          "ubicacionPrivadaPolicias",
+          listaPolicias[obtenerIdUsu(data.codigoSala)].policias
+        );
       } else {
         socket.join(data.idUsu); // ===> Al no coincidir con el código de la sala de la vícitima, crea una sala  nueva para emitir un mensaje de error y después eliminar la misma sala
         io.to(data.idUsu).emit("ubicacionPrivada", "Código inexistente"); // ===> envía mensaje de error
@@ -37,38 +56,52 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("existe_sala", (data, existeSala)=>{
-    const existeSala2 = listaAlertas.some((el) => el.codigoSala === data)
-    existeSala(existeSala2)
-  }
-  )
+  socket.on("existe_sala", (data, existeSala) => {
+    const existeSala2 = listaAlertas.some((el) => el.codigoSala === data);
+    existeSala(existeSala2);
+  });
 
   socket.on("ubicacion_actual", (data) => {
-    if(data.tipoApp === "victima"){
+    if (data.tipoApp === "victima") {
       io.to(data.codigoSala).emit("ubicacionPrivada", data); // ===> envía los datos (lat, long, etc.) del usu a una sala de alerta
-    }else if(data.tipoApp === "policia"){
-      idUsuario = obtenerIdUsu(data.codigoSala)
-      idUsuario !== null ? listaPolicias[idUsuario].policias[data.idDispo].ubicacionFinal = {latitud: data.latitud, longitud: data.longitud} : console.log("error")
-      io.to(`${data.codigoSala}policia`).emit("ubicacionPrivadaPolicias", listaPolicias[obtenerIdUsu(data.codigoSala)].policias); // ===> envía los datos (lat, long, etc.) del policia a una sala de alerta
+    } else if (data.tipoApp === "policia") {
+      idUsuario = obtenerIdUsu(data.codigoSala);
+      idUsuario !== null
+        ? (listaPolicias[idUsuario].policias[data.idDispo].ubicacionFinal = {
+            latitud: data.latitud,
+            longitud: data.longitud,
+          })
+        : console.log("error");
+      io.to(`${data.codigoSala}policia`).emit(
+        "ubicacionPrivadaPolicias",
+        listaPolicias[obtenerIdUsu(data.codigoSala)].policias
+      ); // ===> envía los datos (lat, long, etc.) del policia a una sala de alerta
     }
   });
 
   socket.on("get_room_list", () => {
+    // const sala = io.sockets.adapter.rooms;
+    // console.log(sala);
     io.emit("room_list", listaAlertas); // ===> envío de todas las Salas activas
   });
 
   socket.on("eliminar_sala", (data) => {
     // ===> Filtrar el array para mantener solo las alertas que no tienen el idSala que deseas eliminar
     eliminarAlertas(data);
-    io.to(data).emit("finalizarAlerta", {msj:"Se ha dado de baja su alerta", accion: "baja"})
+    io.to(data).emit("finalizarAlerta", {
+      msj: "Se ha dado de baja su alerta",
+      accion: "baja",
+    });
   });
 
-  const eliminarAlertas = async(codigoSala) => {
-    idUsuario = await obtenerIdUsu(codigoSala)
-    listaAlertas = listaAlertas.filter((alerta) => alerta.codigoSala !== codigoSala); // ===> Funcion para eliminar una Sala
-    idUsuario !== null ? delete listaPolicias[idUsuario]: ()=>{}// ===> Funcion para eliminar lista de policias
+  const eliminarAlertas = async (codigoSala) => {
+    idUsuario = await obtenerIdUsu(codigoSala);
+    io.in(codigoSala).socketsLeave(codigoSala); // Eliminar Sala del socket
+    listaAlertas = listaAlertas.filter(
+      (alerta) => alerta.codigoSala !== codigoSala
+    ); // ===> Funcion para eliminar una Sala de la lista local
+    idUsuario !== null ? delete listaPolicias[idUsuario] : () => {}; //  ===> Funcion para eliminar lista de policias
   };
-
 
   const agregarAlertas = (nombreAlerta) => {
     const existe = listaAlertas.some((el) => el.idUsu === nombreAlerta.idUsu); // ===>controla que el idSala del objeto nuevo no se repita en algún elemento del array listaAlertas
@@ -79,52 +112,56 @@ io.on("connection", (socket) => {
         apellido: nombreAlerta.apellido,
         codigoSala: nombreAlerta.codigoSala,
         latitud: nombreAlerta.latitudInicial,
-        longitud: nombreAlerta.longitudInicial
-      }); 
-      const id = nombreAlerta.idUsu
-      listaPolicias[id] = {codigoSala: nombreAlerta.codigoSala, policias: {}}
+        longitud: nombreAlerta.longitudInicial,
+      });
+      const id = nombreAlerta.idUsu;
+      listaPolicias[id] = { codigoSala: nombreAlerta.codigoSala, policias: {} };
     }
   };
 });
 
-const generarCodigoSala =() => {
+const generarCodigoSala = () => {
   // Generar un número de 4 dígitos al azar
   const codigoSala = Math.floor(1000 + Math.random() * 9000).toString();
   return codigoSala;
-}
+};
 
-const generarCodigoUnico=(array)=>{
+const generarCodigoUnico = (array) => {
   let nuevoCodigo;
-  let existeCodigo = false
+  let existeCodigo = false;
   do {
     // Generar un nuevo código de sala
     nuevoCodigo = generarCodigoSala();
     // Verificar si el nuevo código ya existe en el array
-    existeCodigo = array.some(item => item.codigoSala === nuevoCodigo);
+    existeCodigo = array.some((item) => item.codigoSala === nuevoCodigo);
     // Repetir el proceso si el código ya existe
   } while (existeCodigo);
   return nuevoCodigo;
-}
+};
 
-const obtenerUbiInicial =(codigoSala)=>{
+const obtenerUbiInicial = (codigoSala) => {
   // Buscar el objeto con el código de sala dado
-  const objetoEncontrado = listaAlertas.find(item => item.codigoSala === codigoSala);
+  const objetoEncontrado = listaAlertas.find(
+    (item) => item.codigoSala === codigoSala
+  );
   // Devolver el objeto completo con la ubicación inicial
   return objetoEncontrado;
-}
+};
 
-const obtenerIdUsu =(codigoSala)=>{
- // Buscar el objeto con el código de sala dado
-  const objetoEncontrado = listaAlertas.find(item => item.codigoSala == codigoSala);
- // Devolver el idUsu si se encontró el objeto, o null si no se encontró
- if (objetoEncontrado) {
-  // Devolver el idUsu si se encontró el objeto
-  return objetoEncontrado.idUsu;
-} else {
-  // Devolver null si no se encontró el objeto
-  return null;
-}
-}
+const obtenerIdUsu = (codigoSala) => {
+  // Buscar el objeto con el código de sala dado
+  const objetoEncontrado = listaAlertas.find(
+    (item) => item.codigoSala == codigoSala
+  );
+  // Devolver el idUsu si se encontró el objeto, o null si no se encontró
+  if (objetoEncontrado) {
+    // Devolver el idUsu si se encontró el objeto
+    return objetoEncontrado.idUsu;
+  } else {
+    // Devolver null si no se encontró el objeto
+    return null;
+  }
+};
 
 // const ultimaUbiPolicia = (codigoSala)=>{
 //   let resultado = [];
@@ -153,7 +190,5 @@ const obtenerIdUsu =(codigoSala)=>{
 //   }
 //   return resultado;
 // }
-
-
 
 server.listen(PORT, () => console.log(server.address()));
